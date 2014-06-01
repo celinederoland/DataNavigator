9c2d4c53bee04d3c70c514453743c37d04998535
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-06-01 23:47:03 +0200

    script de positionnement réaliser, reste les valeurs initiales correctes à définir

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,37 @@
 /**
  * Fonctions javascript communes à toute l'application (style graphique en particulier)
  */
+
+$( init );
+ 
+function init() {
+	var larg = $(window).width() - 400;
+	var haut = $(window).height();
+	var hbox = $('#menusite').height() + 20;
+	var lbox = $('#menuvue').width();
+
+	$('#menusite').draggable();
+	$('#menuvue').draggable();
+	$('#menudonnees').draggable();
+	$('#menutools').draggable();
+	$('#recherche').draggable();
+	$('#infos').draggable();
+	$('#options').draggable();
+
+	$('#menusite').css('top','0px');
+	$('#recherche').css('top',hbox + 'px');
+	$('#options').css('top',(2*hbox) + 'px');
+
+	$('#infos').css('top','0px');
+	$('#menudonnees').css('top',hbox + 'px');
+	$('#menuvue').css('top',(2*hbox) + 'px');
+	$('#menutools').css('top',(3*hbox) + 'px');
+
+
+	$('#menuvue').css('left',larg + 'px');
+	$('#menudonnees').css('left',larg + 'px');
+	$('#menutools').css('left',larg + 'px');
+	$('#infos').css('left',larg + 'px');
+
+	$('#infos').resizable({ ghost:true  });
+}
