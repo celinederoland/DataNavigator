7dba1986364376a00f0db460ff05a5819dd4cf61
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-06-02 19:21:05 +0200

    début installation user (marche pas encore)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new Sources\HumourBundle\SourcesHumourBundle(),
             new Relais\RelaisBundle\RelaisRelaisBundle(),
             new Vues\RepresentationsBundle\VuesRepresentationsBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
