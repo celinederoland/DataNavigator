0edf70a0d189987b39409a1c72e7039b5b2473ea
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-06-03 09:53:58 +0200

    maintenant que le userBundle fonctionne, on enlève le menuadmin pour les non admins

--- src/JC/FrontBundle/Tests/Controller/DefaultControllerTest.php
+++ src/JC/FrontBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,6 +13,36 @@ class DefaultControllerTest extends WebTestCase
 		$this->assertTrue($crawler->filter('html:contains("Relations")')->count() > 0,'accueil du site en panne');
 	}
 
+	public function testMenuAdmin()
+	{
+		$client = static::createClient();
+
+		//On se log en non admin, on vérifie que le menu n'y est pas
+		$crawler = $client->request('GET', '/login');
+
+		$form = $crawler -> selectButton('_submit') -> form();
+		$form['_username'] = 'lambda';
+		$form['_password'] = 'lambda';
+		$crawler = $client -> submit($form);
+
+		$crawler = $client -> followRedirect();
+
+		$this->assertTrue($crawler->filter('html:contains("nav#menuadmin")')->count() == 0,'menu admin accessible à un utilisateur lambda');
+
+		//On se log en admin, on vérifie que le menu y est
+		$crawler = $client->request('GET', '/login');
+
+		$form = $crawler -> selectButton('_submit') -> form();
+		$form['_username'] = 'admin';
+		$form['_password'] = 'adminpass';
+		$crawler = $client -> submit($form);
+
+		$crawler = $client -> followRedirect();
+
+		$this->assertTrue($crawler->filter('nav#menuadmin')->count() > 0,'menu admin inaccessible');
+
+	}
+
 	public function testJSTest()
 	{
 		$client = static::createClient();
