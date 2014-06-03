63080c7d9098ae92932597e5eaf01010946bb939
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-06-03 06:49:54 +0200

    login et logout ok

--- src/JC/UserBundle/Tests/Controller/DefaultControllerTest.php
+++ src/JC/UserBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
+	public function testIndex()
+	{
+		$client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+		$crawler = $client->request('GET', '/hello/Fabien');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-    }
+		$this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+	}
+
+	public function testLogin()
+	{
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/login');
+
+		$form = $crawler -> selectButton('_submit') -> form();
+		$form['_username'] = 'lambda';
+		$form['_password'] = 'lambda';
+		$crawler = $client -> submit($form);
+
+		$crawler = $client -> followRedirect();
+
+		$this -> assertTrue($crawler -> filter('html:contains("Bienvenue lambda")') -> count() > 0, 'échec login');
+	}
+
+	public function testLogout()
+	{
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/logout');
+
+		$crawler = $client -> followRedirect();
+
+		$this -> assertTrue($crawler -> filter('html:contains("Bienvenue lambda")') -> count() == 0, 'échec logout');
+	}
 }
