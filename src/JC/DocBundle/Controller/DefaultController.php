94923b72d356c4e8680a80f49e904aa24bf2a7f0
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-06-03 08:28:36 +0200

    tests au vert

--- src/JC/DocBundle/Controller/DefaultController.php
+++ src/JC/DocBundle/Controller/DefaultController.php
@@ -1,13 +1,26 @@
 <?php
+/**
+	* Controleur pour les pages de documentation
+	*
+	* @author Juliana Leclaire <Juliana.Leclaire@etu.univ-savoie.fr>
+	* @author Céline de Roland <Celine.de-Roland@etu.univ-savoie.fr>
+	*
+	* @version = 2.0
+	*/
 
 namespace JC\DocBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+	* Controleur pour les pages de documentation
+	*
+	* @author Juliana Leclaire <Juliana.Leclaire@etu.univ-savoie.fr>
+	* @author Céline de Roland <Celine.de-Roland@etu.univ-savoie.fr>
+	*
+	* @version = 2.0
+	*/
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('JCDocBundle:Default:index.html.twig', array('name' => $name));
-    }
+
 }
--- src/JC/UserBundle/JCUserBundle.php
+++ src/JC/UserBundle/JCUserBundle.php
@@ -1,11 +1,31 @@
 <?php
-
+/**
+	* Classe créée pas Symfony pour gérer le bundle
+	*
+	* @author Juliana Leclaire <Juliana.Leclaire@etu.univ-savoie.fr>
+	* @author Céline de Roland <Celine.de-Roland@etu.univ-savoie.fr>
+	*
+	*/
 namespace JC\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+	* Classe créée pas Symfony pour gérer le bundle
+	*
+	* @author Juliana Leclaire <Juliana.Leclaire@etu.univ-savoie.fr>
+	* @author Céline de Roland <Celine.de-Roland@etu.univ-savoie.fr>
+	*
+	*/
 class JCUserBundle extends Bundle
 {
+
+/**
+	* On indique à symfony que JCUserBundle hérite de FOSUserBundle
+	*
+	* @return String nom du bundle hérité
+	*
+	*/
 	public function getParent()
 	{
 		return 'FOSUserBundle';
