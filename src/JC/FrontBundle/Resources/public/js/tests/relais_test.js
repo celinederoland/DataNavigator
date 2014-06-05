bea26c0d9ba5a7c33d3189f523f3d09e8f291173
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-06-05 15:14:58 +0200

    le mot recherché est pris en compte par la source (testé, documenté)

--- src/JC/FrontBundle/Resources/public/js/tests/relais_test.js
+++ src/JC/FrontBundle/Resources/public/js/tests/relais_test.js
@@ -16,6 +16,7 @@ function relaisTest()
 			$('#wordnet').trigger('click');
 			$('#json').trigger('click');
 			$('#btrecherche').trigger('click');
+			$('#mot').val('machin');
  
 			//Après avoir cliqué sur recherche, on attend 10s pour vérifier le résultat
 			setTimeout(function() {
@@ -28,13 +29,39 @@ function relaisTest()
 
 				//On vérifie que les relations se sont affichées
 				var actualRels = $('#selectRelation').html().length;
-				var expectedRels = 544;
+				var expectedRels = 499;
 				equal(expectedRels,actualRels,'les relations ne sont pas insérées correctement');
 			}, 10000);
 
 		}
 	);
 
+	asyncTest( 
+		"test du passage des options et du mot recherché", 
+		function() {
+			expect( 1 );
+
+			//On active la source et la vue, puis on clique sur recherche
+			$('.activesource').removeClass('activesource');
+			$('.activevue').removeClass('activevue');
+			$('#wordnet').trigger('click');
+			$('#json').trigger('click');
+			$('#btrecherche').trigger('click');
+			$('#limite').val(4);
+			$('#mot').val('machin');
+			$('option').attr('selected','selected');
+ 
+			//Après avoir cliqué sur recherche, on attend 10s pour vérifier le résultat
+			setTimeout(function() {
+				console.log("récupération");
+				var actual = $('#conteneurpage').html().length;
+				var expected = 765;
+				//On vérifie que le résultat est bien une chaîne de caractères ayant la taille attendue
+				equal(expected,actual,'problème dans la prise en compte des options et du mot recherché');
+				start();
+			}, 10000);
+		}
+	);
 }
 
 
