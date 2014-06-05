58335defe13df904a0b71e3fccd5deb7e1379d4b
Céline de Roland (portable) <celine.de-roland@etu.univ-savoie.fr>
2014-06-05 13:29:52 +0200

    ajout de la fonction getRelations dans WordNet

--- src/Sources/WordNetBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Sources/WordNetBundle/Tests/Controller/DefaultControllerTest.php
@@ -7,4 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class DefaultControllerTest extends WebTestCase
 {
 	public function testBidon() { $this -> assertTrue(true); }
+
+	public function testJsonrelations()
+	{
+		$client = static::createClient();
+		$crawler = $client->request('GET', '/fr/wordnet/relations');
+		$expected = '["derivation","pertainymie","construction","participe_passe","hypernymie","hyponymie","meronymie","holonymie","troponymie","verbe_hyponymie","entailments","antonymie","attribut","cause","consequence","similar","synonymie"]';
+		$this->assertEquals($client -> getResponse() -> getContent(), $expected,'wordnet relations en panne');
+	}
 }
